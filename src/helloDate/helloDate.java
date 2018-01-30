01bebe6b9fcd27155f19052f1d85b52507377629
27263 <27263@DESKTOP-D9MDDNJ>
2018-01-30 14:54:04 +0800

    添加注释

--- src/helloDate/helloDate.java
+++ src/helloDate/helloDate.java
@@ -1,8 +1,24 @@
 package helloDate;
 import java.util.*;
+/**
+ * Think in Java 中的第一个程序 Hello Date
+ * page 30
+ * @author Norbert Lee
+ * @version 1.0
+ *
+ */
 public class helloDate {
+	/**
+	 * Entry point to class & application
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown
+	 */
 	public static void main(String[] args) {
 		System.out.print("Hello, it's ");
 		System.out.println(new Date());
 	}
+	/*
+	 * Output
+	 * Hello, it's Tue Jan 30 14:44:27 CST 2018
+	 */
 }
