b0fae4ece0bb6dbe5fe10f909a13fa92af2a05f9
27263 <27263@DESKTOP-D9MDDNJ>
2018-01-30 15:12:28 +0800

    re-manage the menu

---
+++ src/object/helloDate.java
@@ -0,0 +1,26 @@
+package object;
+import java.util.Date;
+/**
+ * Think in Java 
+ * The first example program
+ * Hello Date
+ * page 30
+ * @author Norbert Lee
+ * @version 1.0
+ *
+ */
+public class helloDate {
+	/**
+	 * Entry point to class & application
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello, it's ");
+		System.out.println(new Date());
+	}
+	/*
+	 * Output
+	 * Hello, it's Tue Jan 30 14:44:27 CST 2018
+	 */
+}
