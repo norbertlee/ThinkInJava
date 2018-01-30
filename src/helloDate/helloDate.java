7b036aef0ee54d8e5b5db378f54b5b1c1220a318
norbertlee <272637060@qq.com>
2018-01-30 14:48:08 +0800

    Think in Java Page30 第一个Java程序  helloDate

---
+++ src/helloDate/helloDate.java
@@ -0,0 +1,8 @@
+package helloDate;
+import java.util.*;
+public class helloDate {
+	public static void main(String[] args) {
+		System.out.print("Hello, it's ");
+		System.out.println(new Date());
+	}
+}
